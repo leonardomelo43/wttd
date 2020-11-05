56c2b88d2e2f670bbbfbafb7ed30d48330dc2577
Leonardo Melo <leonardomelo43@gmail.com>
2020-11-05 15:02:04 -0300

    customizaçao das paginas de erro

--- eventex/subscriptions/views.py
+++ eventex/subscriptions/views.py
@@ -16,8 +16,8 @@ def subscribe(request):
 
             mail.send_mail('Confirmação de inscrição', 
                             body,
-                            'contato@eventex.com.br',
-                            ['contato@eventex.com.br', form.cleaned_data['email']])
+                            'leonardomelo43@gmail.com',
+                            ['leonardomelo43@gmail.com', form.cleaned_data['email']])
 
             messages.success(request, 'Inscrição realizada com sucesso!')         
                 
